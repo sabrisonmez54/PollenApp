073e708831eb04d48f361503657c8be4200cea5e
Sabri <sabri.sonmez@macaulay.cuny.edu>
2021-03-21 12:07:35 -0400

    Initial Commit

---
+++ PollenApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PollenApp
+//
+//  Created by Sabri Sönmez on 3/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PollenApp/PollenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PollenAppApp.swift
+//  PollenApp
+//
+//  Created by Sabri Sönmez on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct PollenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
